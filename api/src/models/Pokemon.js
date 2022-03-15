21e9fc53ecffc80b58ecc8c3c1280f1f31ef5ab4
Joaquin Nogueira <89049593+JoaquinNogueira@users.noreply.github.com>
2022-03-15 17:04:57 -0300

    db, back terminado, ultimas cosas front

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,50 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id : {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    hp: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+  },
+  {timestamps: false}); // no quiero que se generen los campos de fecha de creacion y actualizacion;);
 };
---
+++ api/src/routes/pokemons.js
@@ -0,0 +1,22 @@
+/* const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const { Pokemon, Type, Pokemon_Type } = require('../db');
+const { v1: uuidv1, v4: uuidv4 } = require('uuid');
+
+console.log
+console.log(ACAAAAAAAAAAAAAAAAAAAAAAAA)
+
+/* const getApiInfo = async () => {
+
+    try {
+        const infoAxios = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+        console.log(infoAxios.data.results);
+        
+    
+    
+    }catch(error) {
+        console.log(error);
+        }
+    }
+ */ 
